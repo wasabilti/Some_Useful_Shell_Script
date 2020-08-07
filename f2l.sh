fedfddf23a5689d11cc1a422a1db051c83eac2bd
wasabilti <2923043785@qq.com>
2020-08-07 23:23:43 +0800

    fix float bug

--- f2l.sh
+++ f2l.sh
@@ -4,17 +4,21 @@
 # v_p = frac 1 {\sqrt{\mu\epsilon}} = \lambda f
 # $1 frequrncy in GHz unit
 # $2 \epsilon_r default by 1
+#
+digits=3;
 
 if [  -z "$1"  ]
 then
 	echo 'Use ' $0 'frequency \\epsilon_r'
 	exit 1
-elif [ $1 -le 0 ]
+elif [ $(echo "$1 <= 0" | bc) = 1 ]
 then
 	echo 'frequency must greater than zero.'
-elif [ -n "$2" ] && [ $2 -le 0 ]
+	exit 1
+elif [ -n "$2" ] && [ $(echo "$2 <= 0" | bc) = 1 ]
 then
 	echo '\\epsilon_r must greater than zero.'
+	exit 1
 fi
 
 freq=$1;
@@ -23,11 +27,11 @@ freq=$1;
 c=3*10^8
 
 # lambda in free space
-lambda=$(echo "scale=4; $c / ($freq*10^9) * 10^3 " | bc)
+lambda=$(echo "scale=$digits; $c / ($freq*10^9) * 10^3 " | bc)
 echo "lambda = $lambda mm"
 
 # lambda in substrate
-if [ -n "$2" ] && [ $2 -gt 0 ]
+if [ -n "$2" ]
 then
 	eps_r=$2
 else
@@ -35,6 +39,6 @@ else
 fi
 if [ -n "$2" ]
 then
-	lambda=$(echo "scale=4; $c / ($freq*10^9) * 10^3 / sqrt($eps_r)" | bc)
+	lambda=$(echo "scale=$digits; $c / ($freq*10^9) * 10^3 / sqrt($eps_r)" | bc)
 	echo "lambda in substrate = $lambda mm"
 fi
